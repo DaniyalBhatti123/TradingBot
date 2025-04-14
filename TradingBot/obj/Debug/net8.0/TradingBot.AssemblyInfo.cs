35f69de3d57ea545bf8fe0c757f996d8ec54eb17
Daniyal bhatti <40939554+DaniyalBhatti123@users.noreply.github.com>
2025-04-14 09:13:14 +0500

    fixes

--- TradingBot/obj/Debug/net8.0/TradingBot.AssemblyInfo.cs
+++ TradingBot/obj/Debug/net8.0/TradingBot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TradingBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74dcd0eb3ca3e36b351344b0edc4fd547a3c663e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d758abdf81ebbce61e60c623b313c4c29e389810")]
 [assembly: System.Reflection.AssemblyProductAttribute("TradingBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TradingBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
